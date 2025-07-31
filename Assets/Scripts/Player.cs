58bcfc5998e4c3a52062549b5046d6dc0f9ce0f9
MahBestBro <653mOker@gmail.com>
2025-07-31 12:09:06 +1000

    Hamster Z Sorting
    Hamsters now appear in front if their lower down on the screen. This makes a couple of assumptions with how the hamsters walk about and stuff, though it shouldn't be too hard to fix if things do change.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,19 +21,32 @@ public class Player : MonoBehaviour
     {
         Vector2 mouseWorldPos = (Vector2)Camera.main.ScreenToWorldPoint(mousePos.ReadValue<Vector2>());
 
-        if (pickUp.WasPressedThisFrame()) {
-            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, 0.001f * Vector2.right);
-            if (hit) {
-                heldHamster = hit.transform;
-                heldHamsterOffset = (Vector2)hit.transform.position - mouseWorldPos; 
-            } 
+        if (pickUp.WasPressedThisFrame()) 
+        {
+            RaycastHit2D[] hits = Physics2D.LinecastAll(mouseWorldPos, mouseWorldPos + 0.001f * Vector2.right);
+            
+            //NOTE: We consider hamsters that are further down the y-axis to be in front
+            if (hits.Length > 0) 
+            {
+                Transform mostForwardHamster = hits[0].transform;
+                foreach (RaycastHit2D hit in hits) {
+                    if (hit.transform.position.y < mostForwardHamster.position.y) {
+                        mostForwardHamster = hit.transform;
+                    }
+                }
+
+                heldHamster = mostForwardHamster;
+                heldHamsterOffset = (Vector2)mostForwardHamster.position - mouseWorldPos; 
+            }
         }
 
-        if (heldHamster is Transform hamster) {
+        if (heldHamster is Transform hamster) 
+        {
             hamster.position = (Vector3)(mouseWorldPos + heldHamsterOffset);
         }
 
-        if (pickUp.WasReleasedThisFrame()) {
+        if (pickUp.WasReleasedThisFrame()) 
+        {
             heldHamster = null;
         }
     }
