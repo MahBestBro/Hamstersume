cd6751a08b1a2d7edd431a470a786f0e8c74d0f8
MahBestBro <653mOker@gmail.com>
2025-07-31 10:51:11 +1000

    Hamster Pick Up
    Can now pick up and put down hamsters (which are just a white circle for now).

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    InputAction pickUp;
+    InputAction mousePos;
+    
+    Transform? heldHamster = null;
+    Vector2 heldHamsterOffset;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pickUp = InputSystem.actions.FindAction("Pick Up");
+        mousePos = InputSystem.actions.FindAction("Mouse Pos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseWorldPos = (Vector2)Camera.main.ScreenToWorldPoint(mousePos.ReadValue<Vector2>());
+
+        if (pickUp.WasPressedThisFrame()) {
+            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.right);
+            if (hit) {
+                heldHamster = hit.transform;
+                heldHamsterOffset = (Vector2)hit.transform.position - mouseWorldPos; 
+            } 
+        }
+
+        if (heldHamster is Transform hamster) {
+            hamster.position = (Vector3)(mouseWorldPos + heldHamsterOffset);
+        }
+
+        if (pickUp.WasReleasedThisFrame()) {
+            heldHamster = null;
+        }
+    }
+}
