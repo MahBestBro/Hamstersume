68f95fde320f48a60cd5cf5a3a603d62a76b72f7
MahBestBro <653mOker@gmail.com>
2025-08-02 17:08:16 +1000

    Start on Main Menu
    Made a start on the main menu. Currently just got the main, credits, and an empty guide page. These are to be filled later on.

--- Assets/_Scripts/MathHelpers.cs
+++ Assets/_Scripts/MathHelpers.cs
@@ -8,4 +8,13 @@ public static class MathHelpers
         return Mathf.Abs(a - b) <= epsilon;
     }
 
+    public static float ExponentialEaseIn(float x)
+    {
+        return Mathf.Clamp(Mathf.Pow(2.0f, 10.0f * (x - 1.0f)), 0.0f, 1.0f);
+    }
+    
+    public static float ExponentialEaseOut(float x)
+    {
+        return Mathf.Clamp(1.0f - Mathf.Pow(2.0f, -10.0f * x), 0.0f, 1.0f);
+    }
 }
